4f0368b922b4ea0fcc51a226d5c9dff6911b698d
Christian Adamsson <Christian.liu171@gmail.com>
2019-09-09 23:13:15 +0200

    Försökt lägga till jump, funkar ok

--- Assets/Christianasset/scripts/basicmovement.cs
+++ Assets/Christianasset/scripts/basicmovement.cs
@@ -2,21 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityStandardAssets.CrossPlatformInput;
 public class basicmovement : MonoBehaviour
 {
-    public Animator animator;
+    [SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
+    [SerializeField] private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
+    [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
+    [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
+    [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
 
+    private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
+    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    private bool m_Grounded;            // Whether or not the player is grounded.
+    private Transform m_CeilingCheck;   // A position marking where to check for ceilings
+    const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
+    private Animator m_Anim;            // Reference to the player's animator component.
+    private Rigidbody2D m_Rigidbody2D;
+    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+
+    public Animator animator;
+    private bool m_Jump;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_GroundCheck = transform.Find("GroundCheck");
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!m_Jump)
+        {
+            // Read the jump input in Update so button presses aren't missed.
+            m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+        }
+
+
         animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        transform.position = transform.position + horizontal * Time.deltaTime;
+        //float h = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        //m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+        transform.position = transform.position + horizontal * (Time.deltaTime *2);
+        
+
+       
+
+    }
+    private void FixedUpdate()
+    {
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+                m_Grounded = true;
+        }
+        // Read the inputs.
+        bool crouch = Input.GetKey(KeyCode.LeftControl);
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        // Pass all parameters to the character control script.
+        this.Move(h, crouch, m_Jump);
+        m_Jump = false;
+    }
+    public void Move(float h, bool crouch, bool jump)
+    {
+        //m_Rigidbody2D.velocity = new Vector2(h * m_MaxSpeed, m_Rigidbody2D.velocity.y);
+
+        if (m_Grounded && jump) //&& m_Anim.GetBool("Ground"))
+        {
+            // Add a vertical force to the player.
+            m_Grounded = false;
+            //m_Anim.SetBool("Ground", false);
+            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+        }
     }
 }
